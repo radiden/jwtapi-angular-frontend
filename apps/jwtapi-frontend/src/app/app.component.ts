e0dff93547ea4ef76464696c7332d5f24f33bb18
radiden <gabriel2012688@gmail.com>
2021-05-04 16:03:14 +0200

    initial commit

---
+++ apps/jwtapi-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jwtapi-frontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'jwtapi-frontend';
+}
